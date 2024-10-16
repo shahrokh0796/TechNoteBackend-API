8059c219de975581fb6a0e1025c6d98b3577210d
shahrokh0796 <shahrokh07.96@gmail.com>
2024-10-17 01:11:41 +0530

    techNoteBacnkend-api

---
+++ techNodeBackend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'https://misterfasterrepairs.onrender.com',
+    // 'http://localhost:5173',
+    // 'https://misterfasterrepairs.com'
+];
+
+module.exports = allowedOrigins;
