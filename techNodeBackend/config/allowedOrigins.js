1bfe3752baf306049a48884f391be714f70e96ec
shahrokh0796 <shahrokh07.96@gmail.com>
2024-10-21 02:18:16 +0530

    ready to deploy 3

--- techNodeBackend/config/allowedOrigins.js
+++ techNodeBackend/config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'https://misterfasterrepairs.onrender.com',
     // 'http://localhost:5173',
-    // 'https://misterfasterrepairs.com'
 ];
 
 module.exports = allowedOrigins;
