279f0c6bac09dbbf13114e639e7490677a918537
shahrokh0796 <shahrokh07.96@gmail.com>
2024-10-24 11:57:18 +0530

    ready to deploy 4

--- techNodeBackend/models/User.js
+++ techNodeBackend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
-    roles: {type: [String], default: ['Employee']},
+    roles: {type: [String], default: ["Employee"]},
     active: {type: Boolean, default: true}
 });
 
